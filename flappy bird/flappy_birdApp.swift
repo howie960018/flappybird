b633447d2cfc4b8e1b1e706d360b8334252c5bf4
howie960018 <newhowie0427@gmail.com>
2023-03-15 10:39:33 +0800

    Initial Commit

---
+++ flappy bird/flappy_birdApp.swift
@@ -0,0 +1,17 @@
+//
+//  flappy_birdApp.swift
+//  flappy bird
+//
+//  Created by 曾浩儀 on 2023/3/15.
+//
+
+import SwiftUI
+
+@main
+struct flappy_birdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
