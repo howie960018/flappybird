9b3df7bfd801d69ebab5c68feccc62ab5618f51f
howie960018 <newhowie0427@gmail.com>
2023-03-16 07:19:21 +0800

    first demo

---
+++ flappy bird/GameScene.swift
@@ -0,0 +1,8 @@
+//
+//  GameScene.swift
+//  flappy bird
+//
+//  Created by 曾浩儀 on 2023/3/15.
+//
+
+import Foundation
---
+++ flappy bird/GameView.swift
@@ -0,0 +1,20 @@
+//
+//  GameView.swift
+//  flappy bird
+//
+//  Created by 曾浩儀 on 2023/3/15.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
